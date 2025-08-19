1cc850a17d284edf02d1e7c035a05402a5ce7e44
박세연 <152615034+tpdus751@users.noreply.github.com>
2025-08-19 14:57:57 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 200 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,47 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String[] adds = input.nextLine().split("-");
+		
+		int[] ops = new int[adds.length];
+		
+		for (int i = 0; i < adds.length; i++) {
+			if(adds[i].contains("+")) {
+				ops[i] = evaluate(adds[i]);
+			}
+			else {
+				ops[i] = Integer.parseInt(adds[i]);
+			}
+			
+			
+			
+			
+		}
+		int result = ops[0];
+		
+		for (int i = 1; i < ops.length; i++) {
+			result -= ops[i];
+		}
+		System.out.println(result);
+		
+		
+
+	}
+	private static int evaluate(String inStr) {
+		String[] adds = inStr.split("\\+");
+		int sum = 0;
+		
+		for (String add : adds) {
+			sum += Integer.parseInt(add);
+		}
+		
+		return sum;
+	}
+
+}
